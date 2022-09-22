f51c7ccdd43c005000560c2fa0936a24e6a2ea93
Mgs. Tabrani <mgstabrani19@gmail.com>
2022-09-22 13:46:27 +0700

    feature: init shallow machine learning (#3)

--- shallow-machine-learning/main.py
+++ shallow-machine-learning/main.py
@@ -2,6 +2,14 @@ import pandas as pd
 import re
 import nltk
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+from sklearn import svm
+from sklearn.preprocessing import LabelEncoder
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import naive_bayes, svm
+from sklearn.metrics import accuracy_score
 
 train = pd.read_csv("data_worthcheck/train.csv")
 test = pd.read_csv("data_worthcheck/test.csv")
@@ -43,4 +51,27 @@ def preprocess(text):
     processed_text = clean_data(text)
     processed_text = processed_text.lower()
     processed_text = remove_stopwords(processed_text)
-    return processed_text
+    return word_tokenize(processed_text)
+
+for i in range(len(train['text_a'])):
+    train.loc[i, 'processed_text'] = str(preprocess(train.loc[i, 'text_a']))
+
+for i in range(len(test['text_a'])):
+    test.loc[i, 'processed_text'] = str(preprocess(test.loc[i, 'text_a']))
+
+Tfidf_vect = TfidfVectorizer(max_features=5000)
+Tfidf_vect.fit(train['processed_text'])
+Train_X_Tfidf = Tfidf_vect.transform(train['processed_text'])
+Test_X_Tfidf = Tfidf_vect.transform(test['processed_text'])
+
+# Naive Bayes
+Naive = naive_bayes.MultinomialNB()
+Naive.fit(Train_X_Tfidf,train['label'])
+predictions_NB = Naive.predict(Test_X_Tfidf)
+print("Naive Bayes Accuracy Score =",accuracy_score(predictions_NB, test['label'])*100, "%")
+
+# SVM
+SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+SVM.fit(Train_X_Tfidf,train['label'])
+predictions_SVM = SVM.predict(Test_X_Tfidf)
+print("SVM Accuracy Score =",accuracy_score(predictions_SVM, test['label'])*100, "%")
