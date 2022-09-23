a547680bb33c339c4fcc3af98794ede8b4116ea7
Mgs. Tabrani <mgstabrani19@gmail.com>
2022-09-23 07:41:20 +0700

    feature: multiple model with several params shallow ml

--- shallow-machine-learning/main.py
+++ shallow-machine-learning/main.py
@@ -3,17 +3,16 @@ import re
 import nltk
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 from sklearn import svm
-from sklearn.preprocessing import LabelEncoder
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn import naive_bayes, svm
 from sklearn.metrics import accuracy_score
 
 train = pd.read_csv("data_worthcheck/train.csv")
 test = pd.read_csv("data_worthcheck/test.csv")
 dev = pd.read_csv("data_worthcheck/dev.csv")
+
+stopwords_list = set(StopWordRemoverFactory().get_stop_words())
 
 def clean_data(text):
     normal_tw = text
@@ -34,8 +33,6 @@ def clean_data(text):
     normal_tw = re.sub(r'[^\x00-\x7F\x80-\xFF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF]', '', normal_tw)
     
     return normal_tw
-
-stopwords_list = set(StopWordRemoverFactory().get_stop_words())
 
 def remove_stopwords(text):
     token = nltk.word_tokenize(text)
@@ -64,14 +61,43 @@ Tfidf_vect.fit(train['processed_text'])
 Train_X_Tfidf = Tfidf_vect.transform(train['processed_text'])
 Test_X_Tfidf = Tfidf_vect.transform(test['processed_text'])
 
-# Naive Bayes
-Naive = naive_bayes.MultinomialNB()
-Naive.fit(Train_X_Tfidf,train['label'])
-predictions_NB = Naive.predict(Test_X_Tfidf)
-print("Naive Bayes Accuracy Score =",accuracy_score(predictions_NB, test['label'])*100, "%")
-
 # SVM
-SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
-SVM.fit(Train_X_Tfidf,train['label'])
-predictions_SVM = SVM.predict(Test_X_Tfidf)
-print("SVM Accuracy Score =",accuracy_score(predictions_SVM, test['label'])*100, "%")
+SVM1 = svm.SVC(kernel='linear', gamma='auto')
+SVM1.fit(Train_X_Tfidf,train['label'])
+predictions_SVM = SVM1.predict(Test_X_Tfidf)
+print("SVM with linear kernel and auto gamma Accuracy Score =",accuracy_score(predictions_SVM, test['label'])*100, "%")
+
+SVM2 = svm.SVC(kernel='linear', gamma='scale')
+SVM2.fit(Train_X_Tfidf,train['label'])
+predictions_SVM = SVM2.predict(Test_X_Tfidf)
+print("SVM with linear kernel and scale gamma Accuracy Score =",accuracy_score(predictions_SVM, test['label'])*100, "%")
+
+SVM3 = svm.SVC(kernel='rbf', gamma='auto')
+SVM3.fit(Train_X_Tfidf,train['label'])
+predictions_SVM = SVM3.predict(Test_X_Tfidf)
+print("SVM with rbf kernel and auto gamma Accuracy Score =",accuracy_score(predictions_SVM, test['label'])*100, "%")
+
+SVM4 = svm.SVC(kernel='rbf', gamma='scale')
+SVM4.fit(Train_X_Tfidf,train['label'])
+predictions_SVM = SVM4.predict(Test_X_Tfidf)
+print("SVM with rbf kernel and scale gamma Accuracy Score =",accuracy_score(predictions_SVM, test['label'])*100, "%")
+
+SVM5 = svm.SVC(kernel='poly', gamma='auto')
+SVM5.fit(Train_X_Tfidf,train['label'])
+predictions_SVM = SVM5.predict(Test_X_Tfidf)
+print("SVM with poly kernel and auto gamma Accuracy Score =",accuracy_score(predictions_SVM, test['label'])*100, "%")
+
+SVM6 = svm.SVC(kernel='poly', gamma='scale')
+SVM6.fit(Train_X_Tfidf,train['label'])
+predictions_SVM = SVM6.predict(Test_X_Tfidf)
+print("SVM with poly kernel and scale gamma Accuracy Score =",accuracy_score(predictions_SVM, test['label'])*100, "%")
+
+SVM7 = svm.SVC(kernel='sigmoid', gamma='auto')
+SVM7.fit(Train_X_Tfidf,train['label'])
+predictions_SVM = SVM7.predict(Test_X_Tfidf)
+print("SVM with sigmoid kernel and auto gamma Accuracy Score =",accuracy_score(predictions_SVM, test['label'])*100, "%")
+
+SVM8 = svm.SVC(kernel='sigmoid', gamma='scale')
+SVM8.fit(Train_X_Tfidf,train['label'])
+predictions_SVM = SVM8.predict(Test_X_Tfidf)
+print("SVM with sigmoid kernel and scale gamma Accuracy Score =",accuracy_score(predictions_SVM, test['label'])*100, "%")
